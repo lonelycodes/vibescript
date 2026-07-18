dd0a07b7f587b45ed8348fbddbe38e00648a948a
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2026-07-18 19:14:29 +0200

    Add (failing) test for add function

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -29,6 +29,55 @@ func TestBasicFunctionality(t *testing.T) {
 	assertLexer(t, input, tests)
 }
 
+func TestAddFunction(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+let add = |x, y| x + y;
+let result = add(five, ten);
+`
+	tests := []testCase{
+		{token.LET, "let", 1, 1},
+		{token.IDENT, "five", 1, 5},
+		{token.ASSIGN, "=", 1, 10},
+		{token.INT, "5", 1, 12},
+		{token.SEMICOLON, ";", 1, 13},
+
+		{token.LET, "let", 2, 1},
+		{token.IDENT, "ten", 2, 5},
+		{token.ASSIGN, "=", 2, 9},
+		{token.INT, "10", 2, 11},
+		{token.SEMICOLON, ";", 2, 13},
+
+		{token.LET, "let", 3, 1},
+		{token.IDENT, "add", 3, 5},
+		{token.ASSIGN, "=", 3, 9},
+		{token.PIPE, "|", 3, 11},
+		{token.IDENT, "x", 3, 12},
+		{token.COMMA, ",", 3, 13},
+		{token.IDENT, "y", 3, 15},
+		{token.PIPE, "|", 3, 16},
+		{token.IDENT, "x", 3, 18},
+		{token.PLUS, "+", 3, 20},
+		{token.IDENT, "y", 3, 22},
+		{token.SEMICOLON, ";", 3, 23},
+
+		{token.LET, "let", 4, 1},
+		{token.IDENT, "result", 4, 5},
+		{token.ASSIGN, "=", 4, 12},
+		{token.IDENT, "add", 4, 14},
+		{token.LPAREN, "(", 4, 17},
+		{token.IDENT, "five", 4, 18},
+		{token.COMMA, ",", 4, 22},
+		{token.IDENT, "ten", 4, 24},
+		{token.RPAREN, ")", 4, 27},
+		{token.SEMICOLON, ";", 4, 28},
+
+		{token.EOF, "", 5, 1},
+	}
+
+	assertLexer(t, input, tests)
+}
+
 func assertLexer(t *testing.T, input string, tests []testCase) {
 	t.Helper()
 	l := New("test.vibe", input)
