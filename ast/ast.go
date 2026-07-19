c67109cbb0cb0d4f4f2788dd642183e509c2d5a7
Christoph Lauper <christoph.lauper@digitecgalaxus.ch>
2026-07-19 19:13:34 +0200

    Add identifier, let statement

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,10 @@
 package ast
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/lonelycodes/vibescript/token"
+)
 
 type Node interface {
 	TokenLiteral() string
@@ -11,6 +15,84 @@ type Node interface {
 type Statement interface {
 	Node
 	statementNode()
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *Identifier) String() string {
+	return i.Value
+}
+func (i *Identifier) Pos() (int, int) {
+	return i.Token.Line, i.Token.Col
+}
+
+type TypeAnnotation struct {
+	Token    token.Token
+	Name     string
+	Elem     *TypeAnnotation // recursive for list[T] / map[T]
+	Optional bool            // T?
+	Fallible bool            // T! (in return types only)
+}
+
+func (t *TypeAnnotation) expressionNode() {}
+func (t *TypeAnnotation) TokenLiteral() string {
+	return t.Token.Literal
+}
+func (t *TypeAnnotation) Pos() (int, int) {
+	return t.Token.Line, t.Token.Col
+}
+func (t *TypeAnnotation) String() string {
+	var out strings.Builder
+	out.WriteString(t.Name)
+	if t.Elem != nil {
+		out.WriteString("[")
+		out.WriteString(t.Elem.String())
+		out.WriteString("]")
+	}
+	if t.Optional {
+		out.WriteString("?")
+	}
+	if t.Fallible {
+		out.WriteString("!")
+	}
+	return out.String()
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+	Type  *TypeAnnotation
+}
+
+func (ls *LetStatement) statementNode()
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+func (ls *LetStatement) String() string {
+	var out strings.Builder
+	out.WriteString(ls.TokenLiteral())
+	out.WriteString(" ")
+	out.WriteString(ls.Name.String())
+	if ls.Type != nil {
+		out.WriteString(": ")
+		out.WriteString(ls.Type.String())
+	}
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	return out.String()
+}
+func (ls *LetStatement) Pos() (int, int) {
+	return ls.Token.Line, ls.Token.Col
 }
 
 type Expression interface {
